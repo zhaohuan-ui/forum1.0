23d6e38e2162d3e94ec48119e5a2ead4628a4a59
zhaohuan <17614327216@163.com>
2020-05-05 20:33:50 +0800

    修改连接池配置文件 && 文章的保存

--- forum1.0/src/main/java/com/moudles/article/controller/ArticleController.java
+++ forum1.0/src/main/java/com/moudles/article/controller/ArticleController.java
@@ -1,6 +1,12 @@
 package com.moudles.article.controller;
 
 
+import com.commons.utils.HttpResult.HttpResult;
+import com.commons.utils.HttpResult.HttpResultUtil;
+import com.moudles.article.service.impl.ArticleServiceImpl;
+import com.moudles.article.vo.ArticleVo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.stereotype.Controller;
@@ -16,5 +22,13 @@ import org.springframework.stereotype.Controller;
 @Controller
 @RequestMapping("/article")
 public class ArticleController {
+    @Autowired
+    ArticleServiceImpl articleService;
+
+    @RequestMapping("/save")
+    public HttpResult<Object> save(@RequestBody ArticleVo articleVo){
+       articleService.save(articleVo.getArticle(articleVo));
+       return HttpResultUtil.error("success",articleVo);
+    }
 
 }
