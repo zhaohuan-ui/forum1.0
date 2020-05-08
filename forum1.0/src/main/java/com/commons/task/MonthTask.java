d3b154e2d1a55f7b58b5fbbc48f0d7caf0b80871
zhaohuan <17614327216@163.com>
2020-05-08 18:35:58 +0800

    修改配置文件 删除懒加载 定时任务类 添加注解 对定时任务的支持

--- forum1.0/src/main/java/com/commons/task/MonthTask.java
+++ forum1.0/src/main/java/com/commons/task/MonthTask.java
@@ -5,6 +5,7 @@ import com.moudles.article.service.impl.ArticleServiceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -17,6 +18,7 @@ import org.springframework.stereotype.Component;
  * @since 2020-05-08
  */
 @Component
+@EnableScheduling
 public class MonthTask {
 
     private Logger log = LoggerFactory.getLogger(MonthTask.class);
