c01567e409f5a59dfe83a267358e28702fcd5532
zhaohuan <17614327216@163.com>
2020-05-08 18:14:44 +0800

    月调度 删除文章

---
+++ forum1.0/src/main/java/com/commons/task/MonthTask.java
@@ -0,0 +1,33 @@
+package com.commons.task;
+
+import com.commons.utils.date.DateUtils;
+import com.moudles.article.service.impl.ArticleServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/**
+ * <p>
+ * 月定时任务
+ * </p>
+ *
+ * @author zhaohuan
+ * @since 2020-05-08
+ */
+@Component
+public class MonthTask {
+
+    private Logger log = LoggerFactory.getLogger(MonthTask.class);
+
+    @Autowired
+    ArticleServiceImpl articleService;
+
+    //每月最后一日的23:59触发此调度
+    @Scheduled(cron = "0 59 23 L * ?")
+    public void deleteArticles() {
+        articleService.deleteByFlags();
+        log.info("delete articles on {}", DateUtils.getCurrentDateTime());
+    }
+}
--- forum1.0/src/main/java/com/moudles/article/service/impl/ArticleServiceImpl.java
+++ forum1.0/src/main/java/com/moudles/article/service/impl/ArticleServiceImpl.java
@@ -1,10 +1,13 @@
 package com.moudles.article.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.commons.utils.date.DateUtils;
 import com.moudles.article.entity.Article;
 import com.moudles.article.mapper.ArticleMapper;
 import com.moudles.article.service.IArticleService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
+
 
 /**
  * <p>
@@ -28,6 +31,14 @@ public class ArticleServiceImpl extends ServiceImpl<ArticleMapper, Article> impl
             return "success";
         }
         return "error";
+    }
+
+    /**
+     * 物理删除
+     * @return
+     */
+    public void deleteByFlags(){
+        this.removeByIds(this.list(new QueryWrapper<Article>().eq("flags",1).gt("release_time",DateUtils.getCurrentTimestamp(0)-0x18b81ff)));
     }
 
     public Article getArticle(){return new Article();}
